0bdb977a50f3a607e87841b9acfc590a695187a8
kotagiri-preeti <50094402+kotagiri-preeti@users.noreply.github.com>
2020-09-20 12:09:02 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,11 @@
+#Two functions
+def addition(x,y):
+    '''This function for adding two numbers'''#doc string
+    return x+y
+
+def string_concatenation(s):
+    '''This function for concatenating strings'''
+    return "Hello "+ s
+
+#variable
+dict1 = {"name":"preeti","Address":"Hyd"}
